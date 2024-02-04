9329d09e4c2f239313bd49cd169220d5aad0fa78
takhun <thkim1638@gmail.com>
2024-02-04 16:15:30 +0900

    [Initialization] Takhun Kim Blog Customizaiton init

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Takhun Kim",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Data & ML Domain Backend Developer",
+    bio: "I develop everything using Spring and Kubernetes.",
+    email: "takhun.kim.de@gmail.com",
+    linkedin: "TBD",
+    github: "TBD",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `takhunkim-blog`,
+      href: "https://github.com/takhun-kim/takhunkim-blog",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "takhun-blog",
+    description: "welcome Here",
   },
 
   // CONFIG configration (required)
@@ -53,7 +53,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
